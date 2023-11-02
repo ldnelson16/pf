c8f95163671f330ac025e579c9b4f24187cd5bda
ldnelson16 <ldnelson16@gmail.com>
2023-11-02 04:23:18 -0400

    updating page content

---
+++ pages/wordsearch/index.js
@@ -0,0 +1,10 @@
+import HomePage from '../../components/wordsearch/homepage'
+import Boxes from '../../components/boxes'
+
+export default function Page() {
+  return (
+    <>
+      <HomePage></HomePage>
+    </>
+  )
+}
